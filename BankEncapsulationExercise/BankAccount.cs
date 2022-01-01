eacb118bc6bfb5f10d775d5bc971468c27418203
jcrowe91 <jcrowe9119@gmail.com>
2022-01-01 14:36:10 -0600

    Add project files.

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulationExercise
+{
+    class BankAccount
+    {
+
+        private double balance;
+        public BankAccount(double amount)
+        {
+            balance = amount;
+        }
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+
+}
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount checking = new BankAccount(0);
+            Console.WriteLine($"How much would you like to deposit?");
+            double userInput = Convert.ToDouble(Console.ReadLine());
+            checking.Deposit(userInput);
+            Console.WriteLine("Your new balance is...");
+            Console.WriteLine(checking.GetBalance());
+            Console.ReadLine();
+        }
+
+
+
+    }
+}
